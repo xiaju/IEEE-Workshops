3358f37d09d711293c2e654d8e53ee1de2be10e1
Taylor Skilling <skilling.t@husky.neu.edu>
2016-11-08 15:16:17 -0500

    Added HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+// Compile with g++ -o HelloWorld HelloWorld.cpp
+// Run with ./HelloWorld from the command line
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World!\n";
+  std::cout << "Git is the best...\n";
+  std::cout << "Can I see a difference?\n";
+}
