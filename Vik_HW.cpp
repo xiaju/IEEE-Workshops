cb60169cddb77acf09fdf0393fa351a20ea3c5bf
Vikrant Shah <shah.vi@husky.neu.edu>
2016-11-08 18:43:28 -0500

    This is the initial commit from Vik for Vik_HW.cpp

---
+++ Vik_HW.cpp
@@ -0,0 +1,10 @@
+// Compile with g++ -o HelloWorld HelloWorld.cpp
+// Run with ./HelloWorld from the command line
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Vik says Hello World changed 2\n";
+  std::cout << "Git is the best...\n";
+}
